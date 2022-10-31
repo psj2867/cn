c107eb485de2c3aeaf6dd19b8c7ff17df32f96da
psj2867 <psj2867@gmail.com>
2022-10-31 17:14:32 +0900

    sample http server

--- src/main/java/kr/ac/sejong/App.java
+++ src/main/java/kr/ac/sejong/App.java
@@ -1,13 +1,75 @@
 package kr.ac.sejong;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static  void main( String[] args ){
+        App app = new App();
+        app.start();
     }
+    public void start(  )
+    {
+
+        ServerSocket ss = null;
+        Socket socket = null;
+        try {
+            ss = new ServerSocket(8080);
+            System.out.println("서버 시작 :8080 ");
+            while (true) {
+                socket = ss.accept();
+                // readline 함수 쓰기 위해서
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                try(in){
+                    String msg = getUntilNlNl(in);
+                    String response = makeHttpResponse("200 OK", getPath(msg));
+                    send(socket, response);
+                }catch(Exception e){
+                    System.out.println(e.getMessage());
+                }
+                socket.close();
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public String getUntilNlNl(BufferedReader in ) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        while(true){
+            String line = in.readLine();
+            sb.append(line).append("\n");
+            if(line.isBlank() ){
+                return sb.toString();
+            }
+        }
+    }
+
+    private final Pattern pattern = Pattern.compile("^\\S+ (\\S+) \\S+\n");
+
+    public String getPath(String msg){
+        Matcher matcher = pattern.matcher(msg);
+        matcher.find();
+        return matcher.group(1);
+    }
+    public String makeHttpResponse(String code, String msg){
+        return "HTTP/1.0 " + code + " \n\n" + msg;
+    }
+    public void send(Socket socket, String response) throws IOException {
+        OutputStreamWriter writer = new OutputStreamWriter(socket.getOutputStream());
+        writer.write(response);
+        writer.flush();
+    }
+
 }
